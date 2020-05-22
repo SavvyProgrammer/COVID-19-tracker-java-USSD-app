283e0ca829ddd30613daea8752f39bd83e6d27e9
Samson Ehigiator <samson.ehigiator@physci.uniben.edu>
2020-05-22 21:26:53 +0100

    Update LocalTranslation.java

--- src/main/java/app/utilities/LocalTranslation.java
+++ src/main/java/app/utilities/LocalTranslation.java
@@ -8,7 +8,7 @@ import com.google.cloud.translate.TranslateOptions;
 public class LocalTranslation {
 
     static Translate translate = TranslateOptions.newBuilder()
-            .setApiKey("AIzaSyA2Iy_RKiBWy_olSAjZkbtf0oKtgshjfyw") // setApiKey is deprecated, use google service account instead
+            .setApiKey("API key") // setApiKey is deprecated, use google service account instead
             .build()
             .getService();
 
